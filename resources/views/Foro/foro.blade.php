83274c0216021f6b509ed2c1d23c6f77fe21ba7a
LuisLobatoCzs <31270585+LuisLobatoCzs@users.noreply.github.com>
2017-10-24 02:41:56 -0500

    nuevo foro

--- resources/views/Foro/foro.blade.php
+++ resources/views/Foro/foro.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-   
-<form action="">
-<textarea rows="20" cols="40" id="areadetexto">Texto del área de texto</textarea>
-</form>
-        
+<div class="alert alert-success alert-dismissable">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+    <strong>Atención!</strong> Para usar la función de foro debes dar click al enlace "Registrarse" o "Entrar" si ya eres usuario registrado.
 </div>
+	<div class="container">
+		<a id="nabblelink" href="http://biopeb.24017.n8.nabble.com/">BioPEB</a>
+		<script src="http://biopeb.24017.n8.nabble.com/embed/f1"></script>
+	</div>
 @endsection
