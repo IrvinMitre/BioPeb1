fac0174cc95ab3e0575a80d691d7e454f5c3912d
LuisLobatoCzs <31270585+LuisLobatoCzs@users.noreply.github.com>
2017-10-23 23:56:47 -0500

    avisos :v

--- resources/views/Avisos/avisos.blade.php
+++ resources/views/Avisos/avisos.blade.php
@@ -1,18 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<title>Características de los virus</title>
-<head>
-  <meta charset="UTF-8">
-  <title>BioPEP BUAP</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/estilos.css">
-</head>
-
 <style type="text/css"><!--
   section .imagenes-menu img{
     width: 100%;
@@ -47,41 +34,55 @@
 		content: '';
 		display: block;
 		position: absolute;
-		top: 0;
+		top: 100;
 		left: 0;
 		width: 100%;
-		height: 100%;
+		height: 90%;
 		background-image: linear-gradient(to top, rgba(19, 21, 25, 0.5), rgba(19, 21, 25, 0.5)), url("../../images/overlay.png");
 		background-image: url("Imagenes/fondo.jpg");
 		background-size:100% 100%;
 		background-attachment: fixed;
-	}
-
+	}	
 	--></style>
 
+<html xmlns="http://www.w3.org/1999/xhtml">
+<title>Características de los virus</title>
+<head>
+  <meta charset="UTF-8">
+  <title>BioPEP BUAP</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/estilos.css">
+</head>
 <!--Cuerpo de la pagina-->
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.10';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
 <body>
-	<div id="bg"></div>
-	<!--Seccion de actividades-->
 	<section class="main">
-		<div class="container">
-			<div class="imagenes-menu">
-				<article class="row">
-					<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-						<a href="/crucigrama">
-						<img src="{{ asset('Imagenes/cruz.jpg') }}" alt="actividades" class="img-rounded">
-            <img src="{{ asset('Imagenes/test.jpg') }}" alt="actividades" class="img-rounded">
-					</div>
-				</article>
+	<div id="bg">
+	<div class="container">
+			<div style="float:left;width: 50%;">
+				<img src="{{ asset('Imagenes/aviso.jpg') }}" alt="aviso" class="img-rounded">
+			</div>
+			<div style="float:left;width: 50%;">
+				<div class="fb-page" data-href="https://www.facebook.com/Biopeb-938555042959343/" data-tabs="timeline" data-width="500" data-height="600" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Biopeb-938555042959343/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Biopeb-938555042959343/">Biopeb</a></blockquote></div>
 			</div>
 		</div>
+		<div style="clear:both"></div>
 	</section>
-
+	</div>
 	<script src="js/jquery-3.2.1.min.js" charset="utf-8"></script>
 	<script src="bootstrap.min.js" charset="utf-8"></script>
-
 </body>
-
 </html>
 @endsection
